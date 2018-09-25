ec544736c083f39008fc3b6f7916e3605946f12e
Andrei <taysan003@gmail.com>
2018-09-25 15:13:30 -0400

    first tests

--- src/test/java/com.crm.qa.testcases/LoginPageTest.java
+++ src/test/java/com.crm.qa.testcases/LoginPageTest.java
@@ -1,10 +1,17 @@
 package com.crm.qa.testcases;
 
 import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LoginPageTest extends TestBase {
+
+    LoginPage loginPage;
+    HomePage homePage;
 
     public LoginPageTest() {
         super(); // colling constructor TestBase class
@@ -14,6 +21,28 @@ public class LoginPageTest extends TestBase {
     public void setUp(){
         initialization();  //colling driver and go to login page
 
-        LoginPage loginPage = new LoginPage(); // fill in specified field necessary data login and password
+        loginPage = new LoginPage(); // fill in specified field necessary data login and password
+    }
+
+    @Test(priority = 1)
+    public void loginPageTitleTest(){
+        String title = loginPage.validateLoginPageTitle();
+        Assert.assertEquals(title, "#1 Free CRM software in the cloud for sales and service");
+    }
+
+    @Test (priority = 2)
+    public void crmLogoImageTest(){
+        boolean flag =loginPage.validateCRMImage();
+        Assert.assertTrue(flag);
+    }
+
+    @Test (priority = 3)
+    public void loginTest(){
+        homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+    }
+
+    @AfterMethod
+    public void tearDown (){
+        driver.close(); // closing the browser
     }
 }
